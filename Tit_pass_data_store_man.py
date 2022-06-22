97307a817e1da30c3de1a8694c7be0caaf9de5af
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-06-22 20:03:00 +0530

    Create Tit_pass_data_store_man.py

---
+++ Tit_pass_data_store_man.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.DataFrame(
+    {
+        "Name" : [
+                  "Braund, Mr. Owen Harris",
+                  "Allen, Mr. William Henry",
+                  "Bonnel, Miss. Elizabeth",
+
+        ],
+     "Age":[22,35,58],
+     "Sex":["male","male","female"],
+    }
+)
+df
